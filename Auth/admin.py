50b4ed0e624196ef3392288a97ea26f831dd1106
yuvisan99 <sandhu989695@gmail.com>
2025-04-29 21:12:19 +0530

    Add Auth/admin.py 1745941338778

---
+++ Auth/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import User, Course, Result, Subject
+
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Result)
+admin.site.register(Subject)
